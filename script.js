738f86b56a8a4f2cc0234cb302accd0d31cb06e9
iwnlace <94042699+iwnlace@users.noreply.github.com>
2023-09-15 21:46:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+body = document.querySelector("body"),
+sidebar = document.querySelector(".sidebar"),
+toggle = document.querySelector(".toggle"),
+searchBtn = document.querySelector(".search-box"),
+modeSwitch = document.querySelector(".toggle-switch"),
+modeText = document.querySelector(".mode-text");
+
+toggle.addEventListener("click", () =>{
+sidebar.classList.toggle("close");
+});
+
+        
